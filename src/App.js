ee1b80f9fed96d1dbd0d425b893f7c67e54f59c2
jo <jo@jo-300E4A-300E5A-300E7A>
2017-02-14 22:06:15 +0100

    api response

--- src/App.js
+++ src/App.js
@@ -1,21 +1,56 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+
+class Forecast extends Component {
+    constructor() {
+        super();
+
+        this.state = {data: ''};
+        let apiKey = '80e6daa3a673115878aba8e33628330e';
+        this.beforeCityQueryPart = 'http://api.openweathermap.org/data/2.5/forecast/daily?q=';
+        this.afterCityQueryPart = '&mode=json&units=metric&cnt=7&apikey=' + apiKey;
+    }
+
+    componentWillMount() {
+        let self = this;
+        this.query = this.beforeCityQueryPart + this.props.city + this.afterCityQueryPart;
+        axios.get(this.query)
+                .then(function (response) {
+                    //let result = response.data;
+                    //let result = JSON.parse(JSON.stringify(response.data));
+                    let result = JSON.stringify(response.data, null, 4);
+
+                    alert(result);
+                    console.log(result);
+                    self.setState({data: result});
+                })
+
+    }
+
+    render() {
+        let data = this.state.data;
+        return (
+                <div>
+                    <pre>{data}</pre>
+                </div>
+                );
+    }
+}
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+    constructor() {
+        super();
+        this.city = 'Oslo';
+    }
+
+    render() {
+        return (
+                <div>
+                    <Forecast city={this.city} />
+                </div>
+                );
+    }
 }
 
 export default App;
