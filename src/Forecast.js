14c1d1f1ab17e5bbbb086b2e25850bc3e003ceb9
jo <jo@jo-300E4A-300E5A-300E7A>
2017-02-15 10:59:17 +0100

    Forecast

---
+++ src/Forecast.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import './App.css';
+import axios from 'axios';
+
+class Forecast extends React.Component {
+    constructor() {
+        super();
+
+        this.state = {data: ''};
+        let apiKey = '80e6daa3a673115878aba8e33628330e';
+        this.beforeCityQueryPart = 'http://api.openweathermap.org/data/2.5/forecast/daily?q=';
+        this.afterCityQueryPart = '&mode=json&units=metric&cnt=7&apikey=' + apiKey;
+    }
+
+    componentWillMount() {
+        let self = this;
+        this.query = this.beforeCityQueryPart + this.props.city + this.afterCityQueryPart;
+        axios.get(this.query)
+                .then(function (response) {
+                    //let result = response.data;
+                    //let result = JSON.parse(JSON.stringify(response.data));
+                    let result = JSON.stringify(response.data, null, 4);
+
+                    alert(result);
+                    console.log(result);
+                    self.setState({data: result});
+                })
+
+    }
+
+    render() {
+        let data = this.state.data;
+        return (
+                <div>
+                    <pre>{data}</pre>
+                </div>
+                );
+    }
+}
+
+export default Forecast;
--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,44 +1,8 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './App.css';
-import axios from 'axios';
+import Forecast from './Forecast';
 
-class Forecast extends Component {
-    constructor() {
-        super();
-
-        this.state = {data: ''};
-        let apiKey = '80e6daa3a673115878aba8e33628330e';
-        this.beforeCityQueryPart = 'http://api.openweathermap.org/data/2.5/forecast/daily?q=';
-        this.afterCityQueryPart = '&mode=json&units=metric&cnt=7&apikey=' + apiKey;
-    }
-
-    componentWillMount() {
-        let self = this;
-        this.query = this.beforeCityQueryPart + this.props.city + this.afterCityQueryPart;
-        axios.get(this.query)
-                .then(function (response) {
-                    //let result = response.data;
-                    //let result = JSON.parse(JSON.stringify(response.data));
-                    let result = JSON.stringify(response.data, null, 4);
-
-                    alert(result);
-                    console.log(result);
-                    self.setState({data: result});
-                })
-
-    }
-
-    render() {
-        let data = this.state.data;
-        return (
-                <div>
-                    <pre>{data}</pre>
-                </div>
-                );
-    }
-}
-
-class WeatherForecast extends Component {
+class WeatherForecast extends React.Component {
     constructor() {
         super();
         this.city = 'Oslo';
