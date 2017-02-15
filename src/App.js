0d10ad802f623be4ab7e803d3852c7de9cfb2853
jo <jo@jo-300E4A-300E5A-300E7A>
2017-02-15 10:47:11 +0100

    WeatherForecast

--- src/App.js
+++ src/App.js
@@ -1,57 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
-import axios from 'axios';
-
-class Forecast extends Component {
-    constructor() {
-        super();
-
-        this.state = {data: ''};
-        let apiKey = '80e6daa3a673115878aba8e33628330e';
-        this.beforeCityQueryPart = 'http://api.openweathermap.org/data/2.5/forecast/daily?q=';
-        this.afterCityQueryPart = '&mode=json&units=metric&cnt=7&apikey=' + apiKey;
-    }
-
-    componentWillMount() {
-        let self = this;
-        this.query = this.beforeCityQueryPart + this.props.city + this.afterCityQueryPart;
-        axios.get(this.query)
-                .then(function (response) {
-                    //let result = response.data;
-                    //let result = JSON.parse(JSON.stringify(response.data));
-                    let result = JSON.stringify(response.data, null, 4);
-
-                    alert(result);
-                    console.log(result);
-                    self.setState({data: result});
-                })
-
-    }
-
-    render() {
-        let data = this.state.data;
-        return (
-                <div>
-                    <pre>{data}</pre>
-                </div>
-                );
-    }
-}
-
-class WeatherForecast extends Component {
-    constructor() {
-        super();
-        this.city = 'Oslo';
-    }
-
-    render() {
-        return (
-                <div>
-                    <Forecast city={this.city} />
-                </div>
-                );
-    }
-}
+import WeatherForecast from './WeatherForecast';
 
 class App extends Component {
     render() {
