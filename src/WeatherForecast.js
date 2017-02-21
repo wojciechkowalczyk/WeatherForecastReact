8e1fdd86a69fb2d9d35c2cd351fb22405ef60586
jo <jo@jo-300E4A-300E5A-300E7A>
2017-02-21 16:12:42 +0100

    MapPicker

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import Forecast from './Forecast';
 import CityForm from './CityForm';
+import MapPicker from './MapPicker';
 
 class WeatherForecast extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            city: ''
+            city: '',
+            initialCenter: {lng: -90.1056957, lat: 29.9717272}
         };
 
         this.handleUserInput = this.handleUserInput.bind(this);
@@ -22,6 +24,7 @@ class WeatherForecast extends React.Component {
     render() {
         return (
                 <div>
+                    <MapPicker initialCenter={this.state.initialCenter} />
                     <CityForm 
                         city={this.state.city}
                         onSubmit={this.handleUserInput}            
