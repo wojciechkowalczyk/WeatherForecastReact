c4ac74965b92058c8e4a036c1a736d569c3954d2
jo <jo@jo-300E4A-300E5A-300E7A>
2017-02-23 12:50:50 +0100

    set of forecast_chart elements

--- src/ForecastCharts.js
+++ src/ForecastCharts.js
@@ -8,9 +8,15 @@ export default class ForecastCharts extends React.Component {
 
     render() {
         return (
-                <div className="forecast_charts">
-                    <div>
-                        <pre>{this.props.data}</pre>
+                <div>
+                    <div id="temperature_chart" className="forecast_chart"></div>
+                    <div id="percentage_quantities_chart" className="forecast_chart"></div>
+                    <div id="pressure_chart" className="forecast_chart"></div>
+                    
+                    <div className="forecast_charts">
+                        <div>
+                            <pre>{this.props.data}</pre>
+                        </div>
                     </div>
                 </div>
                 );
